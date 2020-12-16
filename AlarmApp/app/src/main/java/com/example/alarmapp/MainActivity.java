3ee731f10f2b94237e13cf70f0f3e4268c0f3e4a
bsh0137 <bookert0137@naver.com>
2020-12-17 01:29:15 +0900

    alarm app start

---
+++ AlarmApp/app/src/main/java/com/example/alarmapp/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.alarmapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ListView list;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        list = (ListView)findViewById(R.id.list);
+
+        List<String> data = new ArrayList<>();
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, data);
+        list.setAdapter(adapter);
+        
+        data.add("알람1");
+        data.add("알람2");
+        data.add("알람3");
+        adapter.notifyDataSetChanged();
+    }
+}
